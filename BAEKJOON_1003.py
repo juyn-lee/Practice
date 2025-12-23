804399d0563feffa50637be555b9dc13d1df9796
Lee Juyeon <juyeon2yo@gmail.com>
2025-12-23 17:48:50 +0900

    12/23

---
+++ BAEKJOON_1003.py
@@ -0,0 +1,61 @@
+# 백준 1003
+# 다음 소스는 N번째 피보나치 수를 구하는 C++ 함수이다.
+# int fibonacci(int n) {
+#     if (n == 0) {
+#         printf("0");
+#         return 0;
+#     } else if (n == 1) {
+#         printf("1");
+#         return 1;
+#     } else {
+#         return fibonacci(n‐1) + fibonacci(n‐2);
+#     }
+# }
+# fibonacci(3)을 호출하면 다음과 같은 일이 일어난다.
+# fibonacci(3)은 fibonacci(2)와 fibonacci(1) (첫 번째 호출)을 호출한다.
+# fibonacci(2)는 fibonacci(1) (두 번째 호출)과 fibonacci(0)을 호출한다.
+# 두 번째 호출한 fibonacci(1)은 1을 출력하고 1을 리턴한다.
+# fibonacci(0)은 0을 출력하고, 0을 리턴한다.
+# fibonacci(2)는 fibonacci(1)과 fibonacci(0)의 결과를 얻고, 1을 리턴한다.
+# 첫 번째 호출한 fibonacci(1)은 1을 출력하고, 1을 리턴한다.
+# fibonacci(3)은 fibonacci(2)와 fibonacci(1)의 결과를 얻고, 2를 리턴한다.
+# 1은 2번 출력되고, 0은 1번 출력된다. N이 주어졌을 때, fibonacci(N)을 호출했을 때, 0과 1이 각각 몇 번 출력되는지 구하는 프로그램을 작성하시오.
+
+# 호출수 문제
+# def fibonacci(N):
+#     global count0, count1
+#     if N == 0:
+#         count0 = count0 + 1
+#         return count0
+#     elif N == 1:
+#         count1 = count1 + 1
+#         return count1
+#     else:
+#         return fibonacci(N-1) + fibonacci(N-2)
+
+# T= int(input())
+
+# for i in range(T):
+#     N= int(input())
+#     count0= 0
+#     count1= 0
+#     if 0 <= N <= 40:
+#         fibonacci(N)
+#     print(count0, count1)
+
+# DP 사용
+T = int(input())
+
+dp0 = [0] * 41
+dp1 = [0] * 41
+
+dp0[0], dp1[0] = 1, 0
+dp0[1], dp1[1] = 0, 1
+
+for i in range(2, 41):
+    dp0[i] = dp0[i-1] + dp0[i-2]
+    dp1[i] = dp1[i-1] + dp1[i-2]
+
+for _ in range(T):
+    N = int(input())
+    print(dp0[N], dp1[N])
