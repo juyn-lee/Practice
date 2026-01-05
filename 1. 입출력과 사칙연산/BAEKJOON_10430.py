7815b785930bd706bab98c2bfa0765f839e2cf84
Lee Juyeon <juyeon2yo@gmail.com>
2026-01-05 11:49:56 +0900

    01/05

---
+++ 1. 입출력과 사칙연산/BAEKJOON_10430.py
@@ -0,0 +1,8 @@
+# 백준 10430
+
+A, B, C= map(int, input().split())
+
+print((A+B)%C)
+print(((A%C)+(B%C))%C)
+print((A*B)%C)
+print(((A%C)*(B%C))%C)
---
+++ 1. 입출력과 사칙연산/BAEKJOON_10869.py
@@ -0,0 +1,9 @@
+# 백준 10869
+
+A, B= map(int, input().split())
+
+print(A+B)
+print(A-B)
+print(A*B)
+print(int(A/B))
+print(A%B)
